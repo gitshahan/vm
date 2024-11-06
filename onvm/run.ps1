21f482f50b95aa24cebf52cc3b46c235a7ef1d15
Shahan Mehboob <shahan@autonicals.org>
2024-11-06 12:56:18 +0000

    removed started vm condition

--- onvm/run.ps1
+++ onvm/run.ps1
@@ -17,12 +17,6 @@ $vmName = "aucscrvm"
 $AzContext = (Connect-AzAccount -Identity).context
 Set-AzContext -SubscriptionId $AzContext.Subscription.Id
 
-# Check VM Status
-$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Status
-if ($vm.PowerState -eq "VM deallocated" -or $vm.PowerState -eq "VM stopped") {
-    Write-Output "Starting VM: $vmName in Resource Group: $resourceGroupName"
-    Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
-    Write-Output "VM started successfully."
-} else {
-    Write-Output "VM is already running. ${vm.PowerState}"
-}
+Write-Output "Starting VM: $vmName in Resource Group: $resourceGroupName"
+Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+Write-Output "VM started successfully."
