aff322f17abe64eaa10fce81e16eb7d80792c72b
Shahan Mehboob <shahan@autonicals.org>
2024-11-06 09:21:18 +0000

    vmon timer function ready

--- onvm/run.ps1
+++ onvm/run.ps1
@@ -9,7 +9,20 @@ if ($Timer.IsPastDue) {
     Write-Host "PowerShell timer is running late!"
 }
 
-# Write an information log with the current time.
-Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
-Start-VM -Name "aucscrvm"
+# Input parameters for the VM
+$resourceGroupName = "SHARED"
+$vmName = "aucscrvm"
 
+# Authenticate with Managed Identity
+$AzContext = (Connect-AzAccount -Identity).context
+Set-AzContext -SubscriptionId $AzContext.Subscription.Id
+
+# Check VM Status
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Status
+if ($vm.PowerState -eq "VM deallocated" -or $vm.PowerState -eq "VM stopped") {
+    Write-Output "Starting VM: $vmName in Resource Group: $resourceGroupName"
+    Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+    Write-Output "VM started successfully."
+} else {
+    Write-Output "VM is already running."
+}
